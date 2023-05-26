145d2489eb16a8b4da4ba163bee012f44f92a47c
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-05-25 23:58:18 -0300

    metodos que vinha do controler, com as determinadas finalidades, como buscar por id, ou uma lista dos componentes que existe...

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,49 @@
+package com.api.parkingcontrol.services;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModels;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+    //@Autowired // esse autowired é dizer que para o spring que vai precisar ser injetado as dependencias do parkingSpotRepository, com toda criação do bin
+    final ParkingSpotRepository parkingSpotRepository;
+    
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+    @Transactional
+    public ParkingSpotModels save(ParkingSpotModels parkingSpotModels){
+        return parkingSpotRepository.save(parkingSpotModels);
+    }
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public List<ParkingSpotModels> findAll(/* Pageable pageable */) {
+        return parkingSpotRepository.findAll(/*pageable*/);
+    }
+
+    public Optional<ParkingSpotModels> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+    @Transactional
+    public void delet(ParkingSpotModels parkingSpotModels) {
+        parkingSpotRepository.delete(parkingSpotModels);
+    }
+}
