7c980b6bb8689fcb1055ce7dd0fb1c6516f87df2
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-09-28 00:52:35 -0300

    criado uma entidade pessoa do apartamento e da garagem

---
+++ src/main/java/com/api/parkingcontrol/models/Person.java
@@ -0,0 +1,69 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Person")
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 20)
+    private String name;
+    @Column(nullable = false, unique = true, length = 10)
+    private String age;
+    @Column(nullable = false, unique = true, length = 10)
+    private String sex;
+    @Column(nullable = false, unique = true, length = 12)
+    private String phone;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
